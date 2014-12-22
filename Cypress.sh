a8f7a44935353ecef313085a368ea354ee0693a5
artem <artem@glitch>
2014-12-22 12:08:13 -0800

    Analysis end of Cypress was added. Should yield matrix output now

--- Cypress.sh
+++ Cypress.sh
@@ -93,8 +93,99 @@ fi
 # Align reads to the the junctions
 	bowtie2-build junction.fa junction
 
-	bowtie2 -x junction -p 2 -1 $NAME.1.fq -2 $NAME.2.fq --end-to-end -a -D 20 -R 3 -N 1 -L 20 -i S,1,0.50 --no-unal -S $NAME.jnc.sam
-	#tophat2 -p 1 -r 50 --report-secondary-alignments -g 1000 -o $PWD $GENOME temp.1.1fq temp.2.fq
+	# High fidelity setting
+	#bowtie2 -x junction -p 2 -1 $NAME.1.fq -2 $NAME.2.fq --end-to-end -a -D 20 -R 3 -N 1 -L 20 -i S,1,0.50 --no-unal -S $NAME.jnc.sam
+
+	# Normal Fidelity
+	#bowtie2 -x junction -p 2 -1 $NAME.1.fq -2 $NAME.2.fq --end-to-end -a -D 15 -R 2 -L 22 -i S,1,1.15 --no-unal -S $NAME.jnc.sam
+
+	# Normal Fidelity only primary alignments
+	bowtie2 -x junction -p 2 -1 $NAME.1.fq -2 $NAME.2.fq --end-to-end -a -D 15 -R 2 -L 22 -i S,1,1.15 --no-unal -S $NAME.jnc.sam
+
+
+	# Convert sam file into a bam file
+	samtools sort -O 'bam' -T tempspace.tmp -o junction.bam $NAME.jnc.sam
+
+	# Retain only primary reads
+	samtools view -F 256 junction.bam -b -o junction.primary.bam
+	samtools index junction.primary.bam
+
+# Analysis of Alignments -------------------------------------------------------
+
+cut -f4 junction.bed | awk -v RL="$READLEN" '{ \
+	print $1 "\t"\
+	RL-5 "\t"\
+	RL+5 "\t"\
+	$1 "\t"\
+	"0\t"\
+	"+\t"\
+ 	"0\t"\
+	"0\t"\
+	"255,0,0\t"\
+	"1\t"\
+	"10\t"\
+	"0\t"}' - > jncCenters.bed.tmp
+
+bedtools coverage -abam junction.primary.bam -b jncCenters.bed.tmp | cut -f1,13 - | sed 's/_/\t/g' - > junctionCoverage.tmp
+
+# awk -v readlen="$READLEN" '{print $1 "\t" readlen - 5 "\t" readlen + 4  "\t" $1}' - 
+
+# Splice Donors
+cut -f1 junctionCoverage.tmp | sort -u - > doner.tmp
+dLen=$(wc -l doner.tmp | cut -f1 -d' ' - ) 
+
+# Splice Acceptors
+cut -f2 junctionCoverage.tmp | sort -u - > accept.tmp
+aLen=$(wc -l accept.tmp | cut -f1 -d' ' - )
+
+# Initilize output matrix
+	rm -f jncMatrix.txt
+	rm -f jncMatrix.tmp
+	touch jncMatrix.tmp
+
+# Iterate through each splice doner as a new line
+for Nd in $(seq 1 $dLen)
+do
+
+	# Initilize output Row for this doner
+	rm -f jncRow.tmp
+	touch jncRow.tmp
+
+	# Extract Splice Doner Name
+	DonerName=$(cut -f1 doner.tmp | sed -n "$Nd"p -)
+
+	# Iterate through each splice accepter as new column
+	for Na in $(seq 1 $aLen)
+	do
+		# Extract Splice Acceptor Name
+		AcceptName=$(cut -f1 accept.tmp | sed -n "$Na"p -)
+
+		# Extract the number of reads over the splice junction
+		LookUp=$(echo "$DonerName\t$AcceptName")
+		SpliceScore=$(grep "$LookUp" junctionCoverage.tmp | cut -f3 - )
+
+		if [ "$SpliceScore" = '' ]
+		then
+			SpliceScore='0'
+		fi
+
+		# Append to row
+		cp jncRow.tmp holder.tmp
+		echo $SpliceScore | paste holder.tmp - > jncRow.tmp
+
+		#echo $DonerName $AcceptName $SpliceScore
+		#cat jncRow.tmp
+		
+	done
+	cat jncRow.tmp
+
+	# Append completed row to matrix output
+	cat jncRow.tmp >> jncMatrix.txt
+done
+
+mv doner.tmp  doner.txt
+mv accept.tmp doner.txt 
+rm *.tmp
 
 
 
