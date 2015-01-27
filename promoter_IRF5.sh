226fcdd61111afceab0fdcd069e506a034470f50
Artem Babaian <ababaian@xhost09.bcgsc.ca>
2015-01-27 15:19:23 -0800

    Running version of promoter analysis is up. Output looks good :)

--- promoter_IRF5.sh
+++ promoter_IRF5.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 #
-# Promoter_IRF5.sh <input jncMatrix.txt> <Library_name>
+# Promoter_IRF5.sh <input.list>
 # Cypress Analysis II
 # Promoter Contribution for IRF5
 
-# INPUT
-#INPUT=$1
-INPUT='jncMatrix.txt'
+# Input List
+INLIST=$1
 
-# Library
-#NAME=$2
-NAME='A0123'
+# Loop ==========================================
+
+# Number of Iterations
+	NUM=$(wc -l $INLIST | cut -f1 -d' ' -)
+
+for X in $(seq $NUM)
+do
+	# Extract line from input.list
+	LINE=$(sed -n "$X"p $INLIST)
+
+	# Output name
+	NAME=$(echo $LINE | cut -f1 -d' ')
+
+	cd $NAME
+
+	# INPUT
+	INPUT='jncMatrix.txt'
 
 # IRF5 Native Promoter
 # Rows: 1,3,4,6-9,23,24
@@ -21,19 +34,25 @@ NAME='A0123'
 # Cols: 2-6
 
 # Generate Native Promoter Matrix
-cut -f 2-5,14 $INPUT | sed -n 1p - > IRF5.native.tmp
-cut -f 2-5,14 $INPUT | sed -n 3,4p - >> IRF5.native.tmp
-cut -f 2-5,14 $INPUT | sed -n 6,9p - >> IRF5.native.tmp
-cut -f 2-5,14 $INPUT | sed -n 23,24p - >> IRF5.native.tmp
+	cut -f 2-5,14 $INPUT | sed -n 1p - > IRF5.native.tmp
+	cut -f 2-5,14 $INPUT | sed -n 3,4p - >> IRF5.native.tmp
+	cut -f 2-5,14 $INPUT | sed -n 6,9p - >> IRF5.native.tmp
+	cut -f 2-5,14 $INPUT | sed -n 23,24p - >> IRF5.native.tmp
 
 # Calculate Native Matrix Sum
 # echo $(RowSum) | ColSum
-NatSum=$(echo $(sed 's/\t/+/g' IRF5.native.tmp | bc)  | sed 's/ /+/g' - | bc)
+	NatSum=$(echo $(sed 's/\t/+/g' IRF5.native.tmp | bc)  | sed 's/ /+/g' - | bc)
 
 # Generate Chimeric Promoter Matrix
-cut -f 2-5,14 $INPUT | sed -n 5p - > IRF5.chim.tmp
-cut -f 2-5,14 $INPUT | sed -n 21,22p - >> IRF5.chim.tmp
+	cut -f 2-5,14 $INPUT | sed -n 5p - > IRF5.chim.tmp
+	cut -f 2-5,14 $INPUT | sed -n 21,22p - >> IRF5.chim.tmp
 
-ChiSum=$(echo $(sed 's/\t/+/g' IRF5.chim.tmp | bc)  | sed 's/ /+/g' - | bc)
+	ChiSum=$(echo $(sed 's/\t/+/g' IRF5.chim.tmp | bc)  | sed 's/ /+/g' - | bc)
 
-echo $NatSum $ChiSum > output.txt
+	echo $NAME $NatSum $ChiSum
+	echo $NatSum $ChiSum > promoter_usage_IRF5.txt
+	rm *.tmp
+
+	cd ..
+
+done
